bb61117aad5c056da62d83dba26041de29f6a4e4
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2021-08-28 00:28:18 +0600

    Item 2 addition

--- showData.py
+++ showData.py
@@ -1,9 +1,8 @@
 from main import *
 
 db.create_all()
-item1 = Item(name = 'Iphone12',price = 500,barcode = '331111234563', description = 'wdffdsfdfdsf' )
-
-
+item1 = Item(name='Iphone12', price=500, barcode='331111234563', description='wdffdsfdfdsf')
+item2 = Item(name= 'Iphone10', price=400, barcode='111111111111', description='noob')
 
 a = Item.query.all()
 print(a)
