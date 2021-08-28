28db03a461fc04acdb5d92c692745fe638f18c6d
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2021-08-28 18:43:51 +0600

    Filtering by price

--- showData.py
+++ showData.py
@@ -6,16 +6,18 @@ item2 = Item(name='Iphone10', price=400, barcode='111111111111', description='no
 item3 = Item(name='Iphone11', price=800, barcode='111111111111', description='this is the right')
 
 # adding data to database
-#db.session.add(item1)
-#db.session.add(item2)
-#db.session.add(item3)
-#db.session.commit()
+# db.session.add(item1)
+# db.session.add(item2)
+# db.session.add(item3)
+# db.session.commit()
 
-# showing all datas
-for item in Item.query.all():
+Item.query.filter_by(price=500)
+for item in Item.query.filter_by(price= 500):
     print(item.name)
-    print(item.price)
-    print(item.description)
-    print(item.id)
-    print(item.barcode)
-
+# showing all datas
+# for item in Item.query.all():
+#    print(item.name)
+#    print(item.price)
+#   print(item.description)
+#    print(item.id)
+#    print(item.barcode)
