7752a354cca3d37111c8c1bcd276e096e5ece1df
Mohammed Adib <76277079+ADIB-the-NOOB@users.noreply.github.com>
2021-08-28 12:04:23 +0600

    third

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Item(db.Model):
 db.create_all()
 
 item1 = Item(name='Iphone12', price=500, barcode='331111234563', description='wdffdsfdfdsf')
-
+item2 = Item(name= 'Iphone10', price=400, barcode='111111111111', description='noob')
 
 
 
--- showData.py
+++ showData.py
@@ -1,8 +1,10 @@
 from main import *
 
-db.create_all()
-item1 = Item(name='Iphone12', price=500, barcode='331111234563', description='wdffdsfdfdsf')
-item2 = Item(name= 'Iphone10', price=400, barcode='111111111111', description='noob')
+# db.session.add(item1)
+# db.session.commit()
+
+db.session.add(item2)
+db.session.commit()
 
 a = Item.query.all()
 print(a)
